39512125e774be2b88224ddbb0f6d5c03a11b4af
Kyle Szeto <kyl3sz3t0@gmail.com>
2017-06-07 08:45:36 -0700

    test seed data stuff

--- test/test-server.js
+++ test/test-server.js
@@ -2,3 +2,30 @@
 const mongoose = require('mongoose');
 const faker = require('faker');
 const chai = require('chai');
+const chaiHttp = require('chai-http');
+const should = chai.should();
+
+const {BlogPost} = require('../models');
+const {TEST_DATABASE_URL} = require('../config');
+const {app,runServer,closeServer} = require('../server');
+
+chai.use(chaiHttp);
+
+function generateBlogs(){
+  return{
+    author:{
+      firstName:faker.name.firstName(),
+      lastName:faker.name.lastName()
+    },
+    content:faker.lorem.paragraph(),
+    title: `${faker.random.bs_adjective()} ${faker.random.bs_noun}`,
+    created: faker.date.recent()
+  };
+}
+function seedData(){
+  const dataArr = [];
+  for(let i = 0; i < 10; i++){
+    dataArr.push(generateBlogs());
+  }
+  return BlogPost.insertMany(dataArr);
+}
